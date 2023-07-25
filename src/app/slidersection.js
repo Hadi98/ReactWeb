86251fff2a22131e903f67ad65ebb63d7d8affaf
Hadi98 <faourhadi0@gmail.com>
2023-07-25 17:25:07 +0300

    first div in the web

---
+++ src/app/slidersection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slidershow from "../../components/Slidershow";
+
+function slidersection(){
+    return (
+        <div>
+            <Slidershow/>
+        </div>
+    );
+}
+
+export default slidersection;
+

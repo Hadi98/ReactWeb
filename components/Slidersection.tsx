5a07036dc898d581185ff5ef8937f0e96eff954c
Zaynab Fayad <zaynab.fayad@cogentsoftech.com>
2023-07-27 15:45:16 +0300

    fix

---
+++ components/Slidersection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Slideshow from "./Slidershow";
+
+export const useClient = true;
+
+function Slidersection() {
+  return (
+    <div className="container mx-auto p-4">
+      <div className="w-full h-80vh">
+        <Slideshow />
+      </div>
+    </div>
+  );
+}
+
+export default Slidersection;
---
+++ components/Slidershow.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'; // Import the required CSS for the slideshow
+
+const Slidershow: React.FC = () => {
+  const images = [
+    "/img2.png",
+    "/img2.png",
+  ];
+
+  const zoomInProperties = {
+    indicators: true,
+    scale: 1,
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+  };
+  
+
+  // Custom styles for the text in the slider
+  const textStyle: React.CSSProperties = {
+    color: 'white',
+    fontSize: '128px',
+    fontWeight: 'bold',
+    lineHeight: '1.3', // Adjust the line height here
+  };
+  
+  return (
+    <div className="m-auto w-full h-full">
+      <Slide {...zoomInProperties}>
+        {images.map((each, index) => (
+          <div key={index} className="each-slide">
+            <div className="flex w-full h-full">
+              {/* Text container */}
+              <div className="flex flex-col justify-center items-start w-1/4 px-4">
+                <p className="font-bold" style={textStyle}>Get Thinking A long time</p>
+                <p className="font-bold" style={{ ...textStyle, fontSize: '16px' }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                </p>
+              </div>
+
+              {/* Image container */}
+              <div className="w-3/4 h-full">
+                <img className="w-full h-full object-cover rounded-lg shadow-xl" src={each} alt={`Slide ${index + 1}`} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slide>
+    </div>
+  );
+};
+
+export default Slidershow;

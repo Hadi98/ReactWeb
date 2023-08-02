e4846377085b286fa070d39a974fabd2f779fce2
Zaynab Fayad <zaynab.fayad@cogentsoftech.com>
2023-08-02 12:30:52 +0300

    fix

--- components/ReadMore.tsx
+++ components/ReadMore.tsx
@@ -7,21 +7,13 @@ interface ReadMoreProps {
 
 const ReadMore: React.FC<ReadMoreProps> = ({ backgroundColor }) => {
   // Define a dynamic className based on the backgroundColor prop
-  const buttonClassName = `px-8 py-4 border-4 ${backgroundColor === 'gray' ? 'border-custom-blue bg-gray-100 text-black' : 'border-white bg-custom-blue text-black'} transition-colors`;
+  const buttonClassName = `px-8 py-4 border-4 rounded-full ${backgroundColor === 'gray' ? 'border-custom-blue bg-gray-100 text-black' : 'border-white bg-custom-blue text-black'} transition-colors`;
 
   // Define the hover effect styles based on the backgroundColor prop
-  const hoverStyle = backgroundColor === 'gray' ? 'hover:bg-blue-400 hover:border-custom-blue hover:text-white' : 'hover:bg-black hover:text-white';
-
-  // Define the border radius based on the backgroundColor prop
-  const borderRadius = backgroundColor === 'gray' ? '12px' : '24px';
-
-  // Define the button style with the border radius
-  const buttonStyle: React.CSSProperties = {
-    borderRadius: borderRadius,
-  };
+  const hoverStyle = backgroundColor === 'gray' ? 'hover:bg-[#00AEEC] hover:border-custom-blue hover:text-white' : 'hover:bg-black hover:text-white';
 
   return (
-    <button className={`${buttonClassName} ${hoverStyle}`} style={buttonStyle}>
+    <button className={`${buttonClassName} ${hoverStyle}`} >
       Read More
     </button>
   );
